504c9f3a55b45e416f7c3531bf4ca88cc32d4bff
石忠华 <shizhonghua@awcloud.com>
2015-08-30 17:37:09 +0800

    首页修改

--- js/common.js
+++ js/common.js
@@ -35,6 +35,32 @@ if(jQuery)(function($){
             maxSlides: 7,
             slideMargin: 10
         });
+
+
+        var index_news_slider = $('.news-slider-list-index').bxSlider({
+            mode: 'fade',
+            captions: true,
+            auto: true,
+            controls:false,
+            autoHover: true,
+            onSlideAfter: function(e, x){
+                // do mind-blowing JS stuff here
+                if(x === 4){
+                    x = -1;
+                }
+                $(".news-img-link-index").find("li.active").removeClass("active");
+                $(".news-img-link-index").find("li:eq("+(x+1)+")").addClass("active");
+            }
+        });
+
+        $(".news-img-link-index").find("li").mouseover(function(e){
+            e.preventDefault();
+            $(".news-img-link-index").find("li.active").removeClass("active");
+            $(this).addClass("active");
+            var idx = $(".news-img-link-index").find("li").index($(this));
+            var gox = idx == 0?4:idx - 1;
+            index_news_slider.goToSlide(gox);
+        })
   })
 		
 	
